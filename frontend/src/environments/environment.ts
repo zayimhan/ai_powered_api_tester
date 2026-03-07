80a854b67736780f6ab0deb7420d86a3526c0d4f
zayimhan <zayimhankorkmaz@gmail.com>
2026-03-07 18:45:15 +0300

    chore: Set prod API URL

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    production: false,
-    apiUrl: 'http://localhost:3000/api',
+    production: true,
+    apiUrl: 'https://ai-powered-api-tester.onrender.com/api',
 };
