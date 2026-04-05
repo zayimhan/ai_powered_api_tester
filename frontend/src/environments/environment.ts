911557c2f10a846b86e8474366728339cebad5fe
zayimhan <zayimhankorkmaz@gmail.com>
2026-04-05 18:10:40 +0300

    change frontend env for new deploy3

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   //apiUrl: 'https://ai-powered-api-tester.onrender.com/api',
-  apiUrl: "https://gentle-integrity-production-8732.up.railway.app",
+  apiUrl: "https://gentle-integrity-production-8732.up.railway.app/api",
 };
